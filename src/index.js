273827df06e9ec9b0e1c6f7d62fdef6945e27489
Marco Pollacci <16632565+marcopollacci@users.noreply.github.com>
2024-05-05 14:43:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 function generateColorTime(){
-    let d = new Date();
-    let color_date = '#' + appendZero(d.getHours()) + appendZero(d.getMinutes()) + appendZero(d.getSeconds())
+    const d = new Date();
+    const color_date = '#' + appendZero(d.getHours()) + appendZero(d.getMinutes()) + appendZero(d.getSeconds())
     document.getElementById('time').innerHTML = appendZero(d.getHours()) + ':' + appendZero(d.getMinutes()) + ':' + appendZero(d.getSeconds());
     document.getElementById('color').innerHTML = color_date;
     document.body.style.backgroundColor = color_date;
 }
 
 
-function appendZero(check){
-    if(check <= 9) {
-        return "0" + check;
-    }
-    return check;
-}
+const appendZero = check => ('0' + check).slice(-2);
 
 window.onload = function (event) {
     window.setInterval(() => {
